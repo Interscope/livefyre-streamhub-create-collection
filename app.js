a98b9e1e84e1fe45d1e1f123ce9f6dd10dd9b93f
MalcolmP <malcolm.poindexter@umusic.com>
2013-09-17 18:30:08 -0700

    reworked for use within module iga-livefyre-convCreator added support for tags & type

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+
 var http = require('http'),
     jwt  = require('jwt-simple'),
     querystring = require('querystring'),
     argv = require('optimist')
-           .usage('Usage: $0 -col [article id] -title [title] -net [network] -site [site id] -secret [site secret] -url [url base] -save [true/false]')
+           .usage('Usage: $0 -col [article id] -title [title] -net [network] -site [site id] -secret [site secret] -url [url base] -tags [tags] -type [type] -lib [true/false] -save [true/false]')
            .demand(['title','net','site','secret','url','col'])
            .alias('title','t')
            .describe('title','livefyre collection title')
@@ -14,49 +15,70 @@ var http = require('http'),
            .describe('secret','livefyre site secret, ex: [user token]')
            .alias('url','u')
            .describe('url','url base, ex: http://demos.livefyre.com/labs-t402/')
-              .alias('save','e')
+           .alias('save','e')
            .describe('save','create(true) test(false)')
            .alias('col','c')
-           .describe('col','livefyre artcile/collection id')
+           .describe('col','livefyre article/collection id')
+           .alias('tags','g')
+           .describe('tags','comma separated list of tags')
+           .alias('type','y')
+           .describe('type','type of collection to create')
+           .alias('lib','l')
+           .describe('lib','run via cli if not true')
            .argv;
 
+var __NETWORK, __SITE_ID, __SITE_SECRET, __SITE_SECRET, __URL_BASE, __SAVE;
 
-var __NETWORK = argv.net; //'labs-t402.fyre.co',
-    __SITE_ID = argv.site; //303827,
-    __SITE_SECRET = argv.secret; //'/=',
-    __URL_BASE = argv.url; //'http://demos.livefyre.com/labs-t402/',
-    __SAVE = (argv.save == 'f' || argv.save == 'false' || argv.save == false ) ? false : true; //true
 
-if( __NETWORK
-    && __SITE_ID
-    && __SITE_SECRET
-    && __URL_BASE
-    && argv.col
-    && argv.title){
-
-    var collection_data = {
-        article_id: argv.col,
-        title: argv.title
-    }
-
-    // console.log(JSON.stringify(collection_data))
-
-    create_collection(collection_data)
+    
+if(!argv.lib){
+	__NETWORK = argv.net; //'labs-t402.fyre.co',
+	__SITE_ID = argv.site; //303827,
+	__SITE_SECRET = argv.secret; //'/=',
+	__URL_BASE = argv.url; //'http://demos.livefyre.com/labs-t402/',
+	__SAVE = (argv.save == 'f' || argv.save == 'false' || argv.save == false ) ? false : true; //true
+	if( __NETWORK
+	    && __SITE_ID
+	    && __SITE_SECRET
+	    && __URL_BASE
+	    && argv.col
+	    && argv.title){
+	
+	    var collection_data = {
+	        articleId: argv.col,
+	        title: argv.title
+	    };
+	    
+	    if(argv.type){
+	    	collection_data.type = argv.type;
+	    }
+	    if(argv.tags){
+	    	collection_data.tags = argv.tags;
+	    }
+	    
+	    // console.log(JSON.stringify(collection_data))
+	
+	    create_collection(collection_data);
+	}
+	else{
+	    console.log("missing data");
+	}
 }
-else{
-    console.log("missing data")
-}
 
-function collection_meta_jwt(site_secret, article_id, title, url, tags){
+function collection_meta_jwt(site_secret, article_id, title, url, tags, type){
     // Create JSON Obj
     data = {
         articleId: article_id,
         title: title,
         url: url
-    }
+    };
     if (tags){
-        data['tags'] = tags
+        data['tags'] = tags;
     }
+    if (type){
+        data['type'] = type;
+    }
+
     return jwt.encode(data, site_secret);
 }
 
@@ -64,22 +86,26 @@ function collection_meta_jwt(site_secret, article_id, title, url, tags){
 function create_collections(infos){
     for(x in infos){
         var info = infos[x];
-        create_collection(info)
+        create_collection(info);
     }
 }
 
-function create_collection(info){
+function create_collection(info, convCallback){
+	var _siteId = __SITE_ID || info["siteId"],
+		_network = __NETWORK || info["network"],
+		_site_secret = __SITE_SECRET || info["secret"],
+		_url = (__URL_BASE) ? __URL_BASE + info['articleId'] : info["url"];
     var post_data = {
-            article_id : info['article_id'],
+            article_id : info['articleId'],
             title: info['title'],
-            url: __URL_BASE + info['article_id'],
-            collectionMeta : collection_meta_jwt(__SITE_SECRET, info['article_id'], info['title'], __URL_BASE + info['article_id'])
-        }
+            url: _url,
+            collectionMeta : collection_meta_jwt(_site_secret, info['articleId'], info['title'], _url, info["tags"], info["type"])
+        };
     var post_data_string = JSON.stringify(post_data);
     var http = require('http');
     var post_options = {
-              host: "quill."+__NETWORK,
-              path: "/api/v3.0/site/"+__SITE_ID+"/collection/create",
+              host: "quill."+_network,
+              path: "/api/v3.0/site/"+_siteId+"/collection/create",
               port: '80',
             method: 'POST',
             headers: {
@@ -88,10 +114,9 @@ function create_collection(info){
             }
         };
 
-
-    if(__SAVE){
+    if( __SAVE !== false && info["save"] !== false ){
         callback = function(response) {
-            var str = ''
+            var str = '';
 
             response.setEncoding('utf-8');
 
@@ -101,20 +126,20 @@ function create_collection(info){
 
             response.on('end', function () {
             console.log(str);
+            convCallback(str);
             });
-        }
+        };
 
         var req = http.request(post_options, callback);
-        req.write(JSON.stringify(post_data))
+        req.write(JSON.stringify(post_data));
         req.end();
     }
     else{
-        console.log(
-            "Creating Collection for article_id="+post_data.article_id+", title="+post_data.title+", url="+post_data.url
-        );
+    	var _msg = "Creating Collection for article_id="+post_data.article_id+", title="+post_data.title+", url="+post_data.url;
+        console.log( _msg );
+        convCallback( post_data );
     }
 }
 
-
-
-
+module.exports = create_collection;
+exports.collection_meta_jwt = collection_meta_jwt;
